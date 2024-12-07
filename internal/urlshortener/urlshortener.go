1c5c3caafd7dd716277a68395babf3914edbaafd
connorpalermo <cjpalermo1996@gmail.com>
2024-12-07 14:48:59 -0500

    chore: update urlshortener to use dynamoDB instead of in memory

--- internal/urlshortener/urlshortener.go
+++ internal/urlshortener/urlshortener.go
@@ -1,80 +1,130 @@
 package urlshortener
 
 import (
+	"errors"
 	"sync"
 
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/connorpalermo/url-shortener/constant/logkey"
+	urlDB "github.com/connorpalermo/url-shortener/internal/persistence"
 	"go.uber.org/zap"
 )
 
 type (
 	UrlShortener struct {
-		Logger             *zap.Logger
-		Mu                 sync.Mutex
-		Counter            int64
-		ShortUrlMapping    map[string]string // TODO: use DB instead
-		OriginalUrlMapping map[string]string
+		Logger   *zap.Logger
+		Mu       sync.Mutex
+		DBClient URLDBProvider
 	}
 
 	UrlShortenerProvider interface {
 		ShortenURL(url string) string
 		GetOriginalURL(shortened string) (string, bool)
 	}
+
+	URLDBProvider interface {
+		GetItemByPK(shortUrl string) (*dynamodb.GetItemOutput, error)
+		WriteItem(id int64, shortUrl, originalUrl string) error
+		IncrementCounter() (int64, error)
+		GetItemByNonPK(attributeName, attributeValue string) (*dynamodb.ScanOutput, error)
+	}
 )
 
-const base62Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const (
+	Base62Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	OriginalURL = "original_url"
+	ShortURL    = "short_url"
+)
 
-func New(logger *zap.Logger) *UrlShortener {
-	return &UrlShortener{
-		Logger:             logger,
-		Counter:            1,
-		ShortUrlMapping:    make(map[string]string),
-		OriginalUrlMapping: make(map[string]string),
+func New(logger *zap.Logger) (*UrlShortener, error) {
+	db, err := urlDB.New(logger)
+	if err != nil {
+		return nil, err
 	}
+
+	return &UrlShortener{
+		Logger:   logger,
+		DBClient: db,
+	}, nil
 }
 
-func (u *UrlShortener) ShortenURL(url string) string {
+func (u *UrlShortener) ShortenURL(url string) (string, error) {
 	u.Mu.Lock()
 	defer u.Mu.Unlock()
 
-	short, ok := u.OriginalUrlMapping[url]
-	if ok {
+	scan, err := u.DBClient.GetItemByNonPK(OriginalURL, url)
+	if err != nil {
+		return "", err
+	}
+
+	if scan.Count > 0 {
 		// we have already seen this URL
-		return short
+		item := scan.Items[0]
+		var attributes map[string]string
+		err = attributevalue.UnmarshalMap(item, &attributes)
+		if err != nil {
+			return "", err
+		}
+
+		shortURL, ok := attributes[ShortURL]
+		if !ok {
+			return "", errors.New("ShortURL attribute missing")
+		}
+
+		return shortURL, nil
 	}
 
 	u.Logger.Info("shortening original URL: ", zap.String(logkey.OriginalURL, url))
 
-	id := u.Counter
-	u.Counter++
+	id, err := u.DBClient.IncrementCounter()
+	if err != nil {
+		return "", err
+	}
 	shortened := encodeBase62(id)
 
+	err = u.DBClient.WriteItem(id, shortened, OriginalURL)
+	if err != nil {
+		return "", err
+	}
 	u.Logger.Info("generated shortened URL: ", zap.String(logkey.ShortenedURL, shortened))
 
-	u.ShortUrlMapping[shortened] = url
-	u.OriginalUrlMapping[url] = shortened
-	return shortened
+	return shortened, nil
 }
 
 func encodeBase62(id int64) string {
 	result := ""
 	for id > 0 {
 		rem := id % 62
-		result = string(base62Chars[rem]) + result
+		result = string(Base62Chars[rem]) + result
 		id /= 62
 	}
 	return result
 }
 
-func (u *UrlShortener) GetOriginalURL(shortened string) (string, bool) {
+func (u *UrlShortener) GetOriginalURL(shortened string) (string, error) {
 	u.Mu.Lock()
 	defer u.Mu.Unlock()
 
 	u.Logger.Info("getting original URL from shortened URL: ", zap.String(logkey.ShortenedURL, shortened))
 
-	original, exists := u.ShortUrlMapping[shortened]
+	original, err := u.DBClient.GetItemByPK(shortened)
+	if err != nil {
+		return "", err
+	}
 
-	u.Logger.Info("retrieved original URL: ", zap.String(logkey.OriginalURL, original))
+	var attributes map[string]string
+	err = attributevalue.UnmarshalMap(original.Item, &attributes)
+	if err != nil {
+		return "", err
+	}
 
-	return original, exists
+	originalURL, ok := attributes[OriginalURL]
+	if !ok {
+		return "", errors.New("OriginalURL attribute missing")
+	}
+
+	u.Logger.Info("retrieved original URL: ", zap.String(logkey.OriginalURL, originalURL))
+
+	return originalURL, nil
 }
--- internal/urlshortener/urlshortener_test.go
+++ internal/urlshortener/urlshortener_test.go
@@ -1,28 +1,108 @@
 package urlshortener
 
 import (
+	"errors"
+	"fmt"
 	"testing"
 
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"go.uber.org/zap"
 )
 
+type MockDBProvider struct {
+	mock.Mock
+}
+
+func (m *MockDBProvider) GetItemByPK(shortUrl string) (*dynamodb.GetItemOutput, error) {
+	args := m.Called(shortUrl)
+	return args.Get(0).(*dynamodb.GetItemOutput), args.Error(1)
+}
+
+func (m *MockDBProvider) WriteItem(id int64, shortUrl, originalUrl string) error {
+	args := m.Called(id, shortUrl, originalUrl)
+	return args.Error(0)
+}
+
+func (m *MockDBProvider) IncrementCounter() (int64, error) {
+	args := m.Called()
+	return args.Get(0).(int64), args.Error(1)
+}
+
+func (m *MockDBProvider) GetItemByNonPK(attributeName, attributeValue string) (*dynamodb.ScanOutput, error) {
+	args := m.Called(attributeName, attributeValue)
+	return args.Get(0).(*dynamodb.ScanOutput), args.Error(1)
+}
+
 func Test_ShortenURL(t *testing.T) {
 	tests := map[string]struct {
-		orignalURLs []string
-		shortURLs   []string
-		count       int64
-		checkCount  bool
+		orignalURL  string
+		scanOutput  *dynamodb.ScanOutput
+		countValue  int64
+		scanError   error
+		writeError  error
+		countError  error
+		shortURL    string
+		expectError bool
 	}{
-		"URL shorten valid": {
-			orignalURLs: []string{"http://www.example.com"},
-			shortURLs:   []string{"b"},
+		"Happy Path URL shorten valid": {
+			orignalURL: "http://www.example.com",
+			scanOutput: &dynamodb.ScanOutput{
+				Count: 0,
+			},
+			countValue: int64(1),
+			shortURL:   "b",
 		},
-		"URL shorten multiple same URL counter does not increase": {
-			orignalURLs: []string{"http://www.example.com", "http://www.example.com", "http://www.example.com", "http://www.example.com", "http://www.example.com"},
-			shortURLs:   []string{"b", "b", "b", "b", "b"},
-			checkCount:  true,
-			count:       int64(2),
+		"Happy Path URL shorten seen before": {
+			orignalURL: "http://www.example.com",
+			scanOutput: &dynamodb.ScanOutput{
+				Count: 1,
+				Items: []map[string]types.AttributeValue{
+					{
+						"original_url": &types.AttributeValueMemberS{Value: "https://example.com"},
+						"short_url":    &types.AttributeValueMemberS{Value: "b"},
+					},
+				},
+			},
+			countValue: int64(1),
+			shortURL:   "b",
+		},
+		"Sad Path Scan Error": {
+			orignalURL:  "http://www.example.com",
+			scanError:   errors.New("error"),
+			expectError: true,
+		},
+		"Sad Path no short_url attribute": {
+			orignalURL: "http://www.example.com",
+			scanOutput: &dynamodb.ScanOutput{
+				Count: 1,
+				Items: []map[string]types.AttributeValue{
+					{
+						"original_url": &types.AttributeValueMemberS{Value: "https://example.com"},
+						"invalid":      &types.AttributeValueMemberS{Value: "b"},
+					},
+				},
+			},
+			expectError: true,
+		},
+		"Sad Path IncrementCount error": {
+			orignalURL: "http://www.example.com",
+			scanOutput: &dynamodb.ScanOutput{
+				Count: 0,
+			},
+			countError:  errors.New("error"),
+			expectError: true,
+		},
+		"Sad Path WriteItem error": {
+			orignalURL: "http://www.example.com",
+			scanOutput: &dynamodb.ScanOutput{
+				Count: 0,
+			},
+			countValue:  int64(1),
+			writeError:  errors.New("error"),
+			expectError: true,
 		},
 	}
 
@@ -32,29 +112,64 @@ func Test_ShortenURL(t *testing.T) {
 			if err != nil {
 				return
 			}
-			u := New(logger)
 
-			for i, orig := range tc.orignalURLs {
-				shortened := u.ShortenURL(orig)
+			m := new(MockDBProvider)
 
-				assert.Equal(t, shortened, tc.shortURLs[i])
+			u := &UrlShortener{
+				Logger:   logger,
+				DBClient: m,
 			}
 
-			if tc.checkCount {
-				assert.Equal(t, tc.count, u.Counter)
+			m.On("WriteItem", mock.Anything, mock.Anything, mock.Anything).Return(tc.writeError)
+			m.On("IncrementCounter").Return(tc.countValue, tc.countError)
+
+			m.On("GetItemByNonPK", mock.Anything, tc.orignalURL).Return(tc.scanOutput, tc.scanError)
+			shortened, err := u.ShortenURL(tc.orignalURL)
+
+			if tc.expectError {
+				assert.Error(t, err)
+				return
 			}
+			assert.Equal(t, shortened, tc.shortURL)
 		})
 	}
 }
 
 func Test_GetOriginalURL(t *testing.T) {
 	tests := map[string]struct {
-		orignalURLs []string
-		shortURLs   []string
+		orignalURL  string
+		shortURL    string
+		itemOutput  *dynamodb.GetItemOutput
+		dbError     error
+		expectError bool
 	}{
-		"URL shorten valid": {
-			orignalURLs: []string{"http://www.example.com"},
-			shortURLs:   []string{"b"},
+		"Happy Path URL exists": {
+			orignalURL: "http://www.example.com",
+			shortURL:   "b",
+			itemOutput: &dynamodb.GetItemOutput{
+				Item: map[string]types.AttributeValue{
+					"id":           &types.AttributeValueMemberN{Value: fmt.Sprintf("%d", int64(1))},
+					"short_url":    &types.AttributeValueMemberS{Value: "b"},
+					"original_url": &types.AttributeValueMemberS{Value: "http://www.example.com"},
+				},
+			},
+		},
+		"Sad Path Get Item By PK error": {
+			orignalURL:  "http://www.example.com",
+			shortURL:    "b",
+			dbError:     errors.New("error"),
+			expectError: true,
+		},
+		"Sad Path No OriginalURL Attribute": {
+			orignalURL: "http://www.example.com",
+			shortURL:   "b",
+			itemOutput: &dynamodb.GetItemOutput{
+				Item: map[string]types.AttributeValue{
+					"id":        &types.AttributeValueMemberN{Value: fmt.Sprintf("%d", int64(1))},
+					"short_url": &types.AttributeValueMemberS{Value: "b"},
+				},
+			},
+			expectError: true,
 		},
 	}
 
@@ -64,14 +179,26 @@ func Test_GetOriginalURL(t *testing.T) {
 			if err != nil {
 				return
 			}
-			u := New(logger)
-
-			for _, orig := range tc.orignalURLs {
-				shortened := u.ShortenURL(orig)
-
-				original, _ := u.GetOriginalURL(shortened)
-				assert.Equal(t, original, orig)
+			m := new(MockDBProvider)
+			m.On("GetItemByPK", tc.shortURL).Return(tc.itemOutput, tc.dbError)
+			u := &UrlShortener{
+				Logger:   logger,
+				DBClient: m,
 			}
+
+			original, err := u.GetOriginalURL(tc.shortURL)
+
+			if tc.expectError {
+				assert.Error(t, err)
+				return
+			}
+			assert.Equal(t, original, tc.orignalURL)
 		})
 	}
+}
+
+func Test_NewClient(t *testing.T) {
+	logger, _ := zap.NewProduction()
+	u, _ := New(logger)
+	u.Logger.Info("Successfully Initialized")
 }
